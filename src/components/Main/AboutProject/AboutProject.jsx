3a937727a63b77f8c836b67938c3858e7342ad45
Paul <paul.ivanov1990@gmail.com>
2023-08-25 16:39:54 +0300

    Start create AboutProject controller

--- src/components/Main/AboutProject/AboutProject.jsx
+++ src/components/Main/AboutProject/AboutProject.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './AboutProject.css';
 
 function AboutProject() {
   return (
-    <>
-      <h2 id="about-project">фыафыа</h2>
-    </>
+    <section id="about-project" className="about-project">
+      <h2 className="about-project__title">О проекте</h2>
+      <div className="about-project__description">
+        <div className="about-project__description-text">
+          <h3 className="about-project__description-title">Дипломный проект включал 5 этапов</h3>
+          <p className="about-project__description-subtitle">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+        </div>
+        <div className="about-project__description-text">
+          <h3 className="about-project__description-title">На выполнение диплома ушло 5 недель</h3>
+          <p className="about-project__description-subtitle">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
